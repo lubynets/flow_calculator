5679ce3fd3a8c2dc76844dc86fa9aabe8660f08b
Lubynets <lubinets95@gmail.com>
2022-06-15 11:50:03 +0200

    develope macros

--- macro/v1_complex.C
+++ macro/v1_complex.C
@@ -1,6 +1,8 @@
 void v1_complex()
 {
-  std::string fileName = "/home/user/cbmdir/working/qna/correlations/cl.dcmqgsm.12agev.defcuts.3122.set2.sgnl_1.root";
+//   std::string fileName = "/home/user/cbmdir/working/qna/correlations/cl.dcmqgsm.12agev.defcuts.3122.set2.sgnl_1.root";
+//   std::string fileName = "/home/user/cbmdir/working/qna/correlations/aXmass/cl.dcmqgsm.apr20.recpid.defaultcuts.3312.set4.root";
+  std::string fileName = "/home/user/cbmdir/working/qna/correlations/aXmass/cl.dcmqgsm.3.3agev.apr20.recpid.lightcuts1.3122.set4.root";
   
   TFile* fileIn = TFile::Open(fileName.c_str());
   
@@ -12,13 +14,13 @@ void v1_complex()
   fileOut->mkdir("v1/usimPsi");
   fileOut->mkdir("v1/uPsi");
   fileOut->mkdir("R1/MC");
-  fileOut->mkdir("R1/3S");
-  fileOut->mkdir("R1/4S_sts_p");
-  fileOut->mkdir("R1/4S_sts_pipos");
+  fileOut->mkdir("R1/sub3");
+  fileOut->mkdir("R1/sub4_sts_p");
+  fileOut->mkdir("R1/sub4_sts_pipos");
   fileOut->mkdir("v1/uQ_R1_MC");
-  fileOut->mkdir("v1/uQ_R1_3S");
-  fileOut->mkdir("v1/uQ_R1_4S_sts_p");
-  fileOut->mkdir("v1/uQ_R1_4S_sts_pipos");
+  fileOut->mkdir("v1/uQ_R1_sub3");
+  fileOut->mkdir("v1/uQ_R1_sub4_sts_p");
+  fileOut->mkdir("v1/uQ_R1_sub4_sts_pipos");
   
   Correlation usimPsi(fileIn, "uPsi", {"u_sim_PLAIN", "Q_psi_PLAIN"}, components);
   usimPsi = usimPsi*2.;
@@ -30,6 +32,11 @@ void v1_complex()
   fileOut->cd("v1/uPsi");
   uPsi.Save("v1.u_rec_RESCALED.Q_psi_PLAIN");
   
+  Correlation usgnlPsi(fileIn, "uPsi", {"u_rec_sgnl_RESCALED", "Q_psi_PLAIN"}, components);
+  usgnlPsi = usgnlPsi*2.;
+  fileOut->cd("v1/uPsi");
+  usgnlPsi.Save("v1.u_rec_sgnl_RESCALED.Q_psi_PLAIN");
+  
   std::vector<Correlation> R1_MC;
   R1_MC.resize(subevents.size());  
   fileOut->cd("R1/MC");
@@ -38,60 +45,77 @@ void v1_complex()
     R1_MC.at(i).Save("res_MC." + subevents.at(i));
   }
   
-  std::vector<Correlation> R1_3S;
-  R1_3S.resize(subevents.size());  
-  fileOut->cd("R1/3S");
+  std::vector<Correlation> R1_sub3;
+  R1_sub3.resize(subevents.size());  
+  fileOut->cd("R1/sub3");
   for(int i=0; i<subevents.size(); i++)
   {
-    R1_3S.at(i) = Functions::VectorResolutions3S( fileIn, "QQ", subevents.at(i), subevents, components ).at(0);
-    R1_3S.at(i).Save("res_3S." + subevents.at(i));
+    R1_sub3.at(i) = Functions::VectorResolutions3S( fileIn, "QQ", subevents.at(i), subevents, components ).at(0);
+    R1_sub3.at(i).Save("res_sub3." + subevents.at(i));
   }
 
-  std::vector<Correlation> R1_4S_sts_p;
-  R1_4S_sts_p.resize(subevents.size());
-  fileOut->cd("R1/4S_sts_p");
+  std::vector<Correlation> R1_sub4_sts_p;
+  R1_sub4_sts_p.resize(subevents.size());
+  fileOut->cd("R1/sub4_sts_p");
   
-  R1_4S_sts_p.at(0) = Functions::VectorResolutions3S( fileIn, "QQ", "psd1_RECENTERED", {"sts_p_RESCALED", "psd3_RECENTERED"}, components ).at(0);
-  R1_4S_sts_p.at(0).Save("res_4S_sts_p." + subevents.at(0));
+  R1_sub4_sts_p.at(0) = Functions::VectorResolutions3S( fileIn, "QQ", "psd1_RECENTERED", {"sts_p_RESCALED", "psd3_RECENTERED"}, components ).at(0);
+  R1_sub4_sts_p.at(0).Save("res_sub4_sts_p." + subevents.at(0));
   
   auto res_sts_p = Functions::VectorResolutions3S( fileIn, "QQ", "sts_p_RESCALED", {"psd1_RECENTERED", "psd3_RECENTERED"}, components ).at(0);
   Correlation psd2_sts_p( fileIn, "QQ", {"psd2_RECENTERED", "sts_p_RESCALED"}, components);
-  R1_4S_sts_p.at(1) = psd2_sts_p/res_sts_p*(-2.);
-  R1_4S_sts_p.at(1).Save("res_4S_sts_p." + subevents.at(1));  
+  R1_sub4_sts_p.at(1) = psd2_sts_p/res_sts_p*(-2.);
+  R1_sub4_sts_p.at(1).Save("res_sub4_sts_p." + subevents.at(1));  
   
-  R1_4S_sts_p.at(2) = Functions::VectorResolutions3S( fileIn, "QQ", "psd3_RECENTERED", {"sts_p_RESCALED", "psd1_RECENTERED"}, components ).at(0);
-  R1_4S_sts_p.at(2).Save("res_4S_sts_p." + subevents.at(2));
+  R1_sub4_sts_p.at(2) = Functions::VectorResolutions3S( fileIn, "QQ", "psd3_RECENTERED", {"sts_p_RESCALED", "psd1_RECENTERED"}, components ).at(0);
+  R1_sub4_sts_p.at(2).Save("res_sub4_sts_p." + subevents.at(2));
   
-  std::vector<Correlation> R1_4S_sts_pipos;
-  R1_4S_sts_pipos.resize(subevents.size());
-  fileOut->cd("R1/4S_sts_pipos");
+  std::vector<Correlation> R1_sub4_sts_pipos;
+  R1_sub4_sts_pipos.resize(subevents.size());
+  fileOut->cd("R1/sub4_sts_pipos");
   
-  R1_4S_sts_pipos.at(0) = Functions::VectorResolutions3S( fileIn, "QQ", "psd1_RECENTERED", {"sts_pipos_RESCALED", "psd3_RECENTERED"}, components ).at(0);
-  R1_4S_sts_pipos.at(0).Save("res_4S_sts_pipos." + subevents.at(0));
+  R1_sub4_sts_pipos.at(0) = Functions::VectorResolutions3S( fileIn, "QQ", "psd1_RECENTERED", {"sts_pipos_RESCALED", "psd3_RECENTERED"}, components ).at(0);
+  R1_sub4_sts_pipos.at(0).Save("res_sub4_sts_pipos." + subevents.at(0));
   
   auto res_sts_pipos = Functions::VectorResolutions3S( fileIn, "QQ", "sts_pipos_RESCALED", {"psd1_RECENTERED", "psd3_RECENTERED"}, components ).at(0);
   Correlation psd2_sts_pipos( fileIn, "QQ", {"psd2_RECENTERED", "sts_pipos_RESCALED"}, components);
-  R1_4S_sts_pipos.at(1) = psd2_sts_pipos/res_sts_pipos*(-2.);
-  R1_4S_sts_pipos.at(1).Save("res_4S_sts_pipos." + subevents.at(1));  
+  R1_sub4_sts_pipos.at(1) = psd2_sts_pipos/res_sts_pipos*(-2.);
+  R1_sub4_sts_pipos.at(1).Save("res_sub4_sts_pipos." + subevents.at(1));  
   
-  R1_4S_sts_pipos.at(2) = Functions::VectorResolutions3S( fileIn, "QQ", "psd3_RECENTERED", {"sts_pipos_RESCALED", "psd1_RECENTERED"}, components ).at(0);
-  R1_4S_sts_pipos.at(2).Save("res_4S_sts_pipos." + subevents.at(2));
+  R1_sub4_sts_pipos.at(2) = Functions::VectorResolutions3S( fileIn, "QQ", "psd3_RECENTERED", {"sts_pipos_RESCALED", "psd1_RECENTERED"}, components ).at(0);
+  R1_sub4_sts_pipos.at(2).Save("res_sub4_sts_pipos." + subevents.at(2));
   
   for(int i=0; i<subevents.size(); i++) {
     Correlation uQ(fileIn, "uQ", {"u_rec_RESCALED", subevents.at(i)}, components);
     auto uQ_R1_MC = uQ / R1_MC.at(i) * 2.;
-    auto uQ_R1_3S = uQ / R1_3S.at(i) * 2.;
-    auto uQ_R1_4S_sts_p = uQ / R1_4S_sts_p.at(i) * 2.;
-    auto uQ_R1_4S_sts_pipos = uQ / R1_4S_sts_pipos.at(i) * 2.;
-    
+    auto uQ_R1_sub3 = uQ / R1_sub3.at(i) * 2.;
+    auto uQ_R1_sub4_sts_p = uQ / R1_sub4_sts_p.at(i) * 2.;
+    auto uQ_R1_sub4_sts_pipos = uQ / R1_sub4_sts_pipos.at(i) * 2.;
+        
     fileOut->cd("v1/uQ_R1_MC");
     uQ_R1_MC.Save("v1.u_rec_RESCALED." + subevents.at(i) + ".res_MC");
-    fileOut->cd("v1/uQ_R1_3S");
-    uQ_R1_3S.Save("v1.u_rec_RESCALED." + subevents.at(i) + ".res_3S");
-    fileOut->cd("v1/uQ_R1_4S_sts_p");
-    uQ_R1_4S_sts_p.Save("v1.u_rec_RESCALED." + subevents.at(i) + ".res_4S_sts_p");
-    fileOut->cd("v1/uQ_R1_4S_sts_pipos");
-    uQ_R1_4S_sts_pipos.Save("v1.u_rec_RESCALED." + subevents.at(i) + ".res_4S_sts_pipos");
+    fileOut->cd("v1/uQ_R1_sub3");
+    uQ_R1_sub3.Save("v1.u_rec_RESCALED." + subevents.at(i) + ".res_sub3");
+    fileOut->cd("v1/uQ_R1_sub4_sts_p");
+    uQ_R1_sub4_sts_p.Save("v1.u_rec_RESCALED." + subevents.at(i) + ".res_sub4_sts_p");
+    fileOut->cd("v1/uQ_R1_sub4_sts_pipos");
+    uQ_R1_sub4_sts_pipos.Save("v1.u_rec_RESCALED." + subevents.at(i) + ".res_sub4_sts_pipos");
+  }
+  
+  for(int i=0; i<subevents.size(); i++) {
+    Correlation uQ(fileIn, "uQ", {"u_rec_sgnl_RESCALED", subevents.at(i)}, components);
+    auto uQ_R1_MC = uQ / R1_MC.at(i) * 2.;
+    auto uQ_R1_sub3 = uQ / R1_sub3.at(i) * 2.;
+    auto uQ_R1_sub4_sts_p = uQ / R1_sub4_sts_p.at(i) * 2.;
+    auto uQ_R1_sub4_sts_pipos = uQ / R1_sub4_sts_pipos.at(i) * 2.;
+        
+    fileOut->cd("v1/uQ_R1_MC");
+    uQ_R1_MC.Save("v1.u_rec_sgnl_RESCALED." + subevents.at(i) + ".res_MC");
+    fileOut->cd("v1/uQ_R1_sub3");
+    uQ_R1_sub3.Save("v1.u_rec_sgnl_RESCALED." + subevents.at(i) + ".res_sub3");
+    fileOut->cd("v1/uQ_R1_sub4_sts_p");
+    uQ_R1_sub4_sts_p.Save("v1.u_rec_sgnl_RESCALED." + subevents.at(i) + ".res_sub4_sts_p");
+    fileOut->cd("v1/uQ_R1_sub4_sts_pipos");
+    uQ_R1_sub4_sts_pipos.Save("v1.u_rec_sgnl_RESCALED." + subevents.at(i) + ".res_sub4_sts_pipos");
   }
   
   fileOut->Close();
